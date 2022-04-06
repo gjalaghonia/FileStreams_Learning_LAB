75e77d9bf6f815001295c2b21fbc1ed71e89d672
gjalaghonia <g.jalaghonia@live.com>
2022-04-06 23:37:35 +0400

    Add project files.

---
+++ FileHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file
+{
+	static class FileHelper
+	{
+		public static int Countwords(string inputFilePath)
+		{
+            int index = 0;
+			int counter = 0;
+            string countText = ReadText(inputFilePath);
+			while (index < countText.Length-1)
+			{
+				if (countText[index] == ' ' || countText[index] == '\n' || countText[index] == '\r' || countText[index] == '\t')
+				{
+					counter++;
+				}
+				index++;
+			}
+            return counter;
+     	}
+		public static void EnumerateFile(string inputFilePath, string outputFilePath)
+		{
+			string inputText = ReadText(inputFilePath);
+			string outputText = "";
+
+			int index = 1;
+			outputText += index.ToString() + ". ";
+			for (int i = 0; i < inputText.Length; i++)
+			{
+				if (inputText[i] == '\r')
+				{
+					index++;
+					outputText += "\r\n";
+					outputText += index.ToString() + ". ";
+					i++;
+					continue;
+				}
+				outputText += inputText[i];
+			}
+
+			WriteText(outputFilePath, outputText);
+		}
+
+		public static string ReadText(string path)
+		{
+			FileStream fileStream = new FileStream(path, FileMode.Open);
+			byte[] data = new byte[fileStream.Length];
+			fileStream.Read(data, 0, data.Length);
+			fileStream.Close();
+
+			return GetString(data);
+		}
+
+		public static void WriteText(string path, string text)
+		{
+			FileStream fileStream = new FileStream(path, FileMode.Create);
+			byte[] data = GetBytes(text);
+			fileStream.Write(data, 0, data.Length);
+			fileStream.Close();
+		}
+
+		private static byte[] GetBytes(string text)
+		{
+			byte[] buffer = new byte[text.Length];
+			for (int i = 0; i < text.Length; i++)
+			{
+				buffer[i] = (byte)text[i];
+			}
+			return buffer;
+		}
+
+		private static string GetString(byte[] data)
+		{
+			char[] buffer = new char[data.Length];
+			for (int i = 0; i < data.Length; i++)
+			{
+				buffer[i] = (char)data[i];
+			}
+			return new string(buffer);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace file
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            //FileHelper.WriteText(@"C:\Users\Guram\Desktop\TEST\G10.txt", "text1   text2 text3\r\ntext4 text5\r\ntext6 text7 text8 text9\r\ntext9 text10");
+            //FileHelper.EnumerateFile(@"C:\Users\Guram\Desktop\TEST\G10.txt", @"C:\Users\Guram\Desktop\TEST\G10_2.txt");
+            //int countwords = FileHelper.Countwords(@"C:\Users\Guram\Desktop\TEST\G10.txt");
+            ////Console.WriteLine(countwords);
+            //Console.WriteLine(symbolcount);
+            string readtext = FileHelper.ReadText(@"C:\Users\Guram\Desktop\TEST\G10.txt");
+            ArrayList arrayList = new ArrayList()
+            { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+            while (readtext.Length > 0)
+            {
+                Console.WriteLine(readtext[0] + "-----"); // charebis 1 elementi: ASCI
+                int temcount = 0;
+                for (int i = 0; i < readtext.Length; i++)
+                {
+                    if (readtext[0] == readtext[i] && arrayList.Contains(readtext[0]))
+                    {
+                        temcount++;
+                    }
+                }
+                Console.WriteLine(temcount);
+                readtext = readtext.Replace(readtext[0].ToString(), string.Empty);
+            }
+
+        }
+    }
+
+}
